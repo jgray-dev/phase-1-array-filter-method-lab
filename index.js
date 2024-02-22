478f2a33a3c43ac3a9ead9810949e3e656c8bd27
Jackson Gray <nohaxjustdoge@gmail.com>
2024-02-22 04:44:16 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const newarr = []
+    drivers.forEach((driver)=> {
+        (driver.toLowerCase() === string.toLowerCase()) ? newarr.push(driver) : undefined
+    })
+    return newarr
+}
+
+function fuzzyMatch(drivers, string) {
+    const newarr = []
+    drivers.forEach((driver)=> {
+        driver.startsWith(string) ? newarr.push(driver) : undefined
+    })
+    return newarr
+}
+
+function matchName(drivers, string) {
+    const newarr = []
+    drivers.forEach((driver)=> {
+        driver.name === string ? newarr.push(driver) : undefined
+        //(driver.name.toLowerCase() === string.toLowerCase()) ? newarr.push(driver.name) : undefined
+        // WHY DOESNT THIS WORK??? ^^
+    })
+    return newarr
+}
